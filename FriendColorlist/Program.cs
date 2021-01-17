15571a41e9026624ed348efa9ec346bf7380453b
Dima <dimazhykovskiy@gmail.com>
2021-01-17 17:38:51 +0200

    added output and repeatability

--- FriendColorlist/Program.cs
+++ FriendColorlist/Program.cs
@@ -8,33 +8,37 @@ namespace FriendColorlist
         {
 
             string[] names = new string[6] { "Настя", "Назар", "Дима", "Эрвин", "Исмаил", "Влад" };
-            string[] colors = new string[6] { "Белый", "Чёрный", "Фиолетовый", "unknown", "Оранжевый", "Зелёный" };
+            string[] colors = new string[6] { "Белый", "Чёрный", "Фиолетовый", "Бирюзовый", "Оранжевый", "Зелёный" };
 
             Console.WriteLine("Чей любимый цвет вам интересно узнать?");
             Console.WriteLine("Введите число под которым находится этот человек.");
+            Console.WriteLine("Если хотите выйти пропишите - exit");
 
             for (int i = 0; i < names.Length; i++)
             {
                 Console.WriteLine($"{i + 1}. {names[i]}");
             }
-
-
-            int number;
             
             while (true)
             {
                 string answer = Console.ReadLine();
 
-                if (int.TryParse(answer, out int a) && a <= names.Length + 1)
+                if (answer == "exit")
                 {
-                    number = a;
+                    Console.WriteLine("До свидания");
                     break;
                 }
 
-                Console.WriteLine("Что-то пошло не так, попробуйте ещё раз.");
+                else if (int.TryParse(answer, out int a) && a < names.Length + 1 && a > 0)
+                {
+                    Console.WriteLine($"Любимый цвет этого пользователя - {colors[a - 1]}");
+                }
+                else
+                {
+                    Console.WriteLine("Что-то пошло не так");
+                    continue;
+                }
             }
-
-            Console.WriteLine($"\nЛюбимый цвет этого пользователя - {colors[number - 1]}");
 
             Console.ReadKey();
         }
