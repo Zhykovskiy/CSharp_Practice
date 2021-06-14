0415f3d91393378a626ca370f731e684b3266fe5
Dima <dimazhykovskiy@gmail.com>
2021-06-14 18:08:19 +0300

    Calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Площадь треугольника по формуле Герона: {Triangle.GetTriangleSquare(4.3, 6.9, 3.7)}");
+            Console.WriteLine($"Площадь треугольника по высоте и основе: {Triangle.GetTriangleSquare(5, 7)}");
+            Console.WriteLine($"Площадь треугольника по двум смежным сторонам и углу между ними: {Triangle.GetTriangleSquare(4, 6, 90)}");
+        }
+    }
+}
---
+++ Calculator/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class Triangle
+    {
+        public static double GetTriangleSquare(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public static double GetTriangleSquare(double a, double h)
+        {
+            return 0.5 * a * h;
+        }
+
+        public static double GetTriangleSquare(double a, double b, int alpha)
+        {
+            return 0.5 * a * b * Math.Sin(alpha);
+        }
+    }
+}
