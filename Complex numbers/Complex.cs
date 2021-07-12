890dc28b1beb1d809e8e8a1a5c9a762dda9f3cb7
Zhukovskiy Dmytro <dimazhykovskiy@gmail.com>
2021-07-12 17:58:02 +0300

    Initial commit

---
+++ Complex numbers/Complex.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex_numbers
+{
+    public class Complex
+    {
+        public double Real { get; private set; }
+        public double Imaginary { get; private set; }
+
+        public Complex(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        public Complex Plus(Complex number)
+        {
+            return new Complex(Imaginary + number.Imaginary, Real + number.Real);
+        }
+
+        public Complex Minus(Complex number)
+        {
+            return new Complex(Imaginary - number.Imaginary, Real - number.Real);
+        }
+    }
+}
---
+++ Complex numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Complex_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex(1, 1);
+            Complex c2 = new Complex(1, 2);
+
+            Complex result = c1.Plus(c2);
+
+            Console.WriteLine($"Result.Real = {result.Real}; Result.Imaginary = {result.Imaginary}");
+        }
+    }
+}
