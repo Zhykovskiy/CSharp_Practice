ca4f8f88171127b81feb914807382e9a873b9d30
Zhukovskiy Dmytro <dimazhykovskiy@gmail.com>
2021-07-12 21:11:01 +0300

    Initial commit

---
+++ Guess the number Game/Player.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess_the_number_Game
+{
+    public interface IPlayer
+    {
+        void Guess(int number, int lives);
+        int MakeAGuess();
+    }
+
+    public class Human : IPlayer
+    {
+        public void Guess(int number, int lives)
+        {
+            bool end = false;
+
+            while (!end)
+            {
+                int answer = int.Parse(Console.ReadLine());
+
+                if (answer == number)
+                {
+                    end = true;
+                    Console.WriteLine("Поздарвляю, Вы выиграли!");
+                }
+                else if (lives == 1)
+                {
+                    end = true;
+                    Console.WriteLine("Попытки закончились! Вы проиграли!" +
+                        $"Правильный ответ: {number}");
+                }
+                else
+                {
+                    Console.WriteLine("Неверно!");
+                    Console.WriteLine($"Осталось попыток: {--lives}");
+                    if (answer < number)
+                    {
+                        Console.WriteLine("Загаданное число больше");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Загаданное число меньше");
+                    }
+                }
+            }
+        }
+
+        public int MakeAGuess()
+        {
+            Console.WriteLine("Введите число от 0 до 100: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            return number;
+        }
+    }
+
+    class Computer : IPlayer
+    {
+        public void Guess(int number, int lives)
+        {
+            bool end = false;
+            int rangeX = 0, rangeY = 100;
+
+            while (!end)
+            {
+                int answer = (rangeX + rangeY) / 2;
+                Console.WriteLine($"Машина предполагает что это {answer}");
+                if (answer == number)
+                {
+                    end = true;
+                    Console.WriteLine("Поздарвляю, Вы выиграли!");
+                }
+                else if (lives == 1)
+                {
+                    end = true;
+                    Console.WriteLine("Попытки закончились! Вы проиграли!" +
+                        $"Правильный ответ: {number}");
+                }
+                else
+                {
+                    Console.WriteLine("Неверно!");
+                    Console.WriteLine($"Осталось попыток: {--lives}");
+                    if (answer < number)
+                    {
+                        rangeX = answer;
+                    }
+                    else
+                    {
+                        rangeY = answer;
+                    }
+                }
+            }
+        }
+
+        public int MakeAGuess()
+        {
+            Random rand = new Random();
+            int number = rand.Next(100);
+
+            return number;
+        }
+    }
+}
---
+++ Guess the number Game/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Guess_the_number_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите кто загадает число:\n" +
+                "1. Человек\n" +
+                "2. Машина");
+            int choise = int.Parse(Console.ReadLine());
+            int number, lives = 5;
+
+            IPlayer player1, player2;
+
+            if (choise == 1)
+            {
+                player1 = new Human();
+            }
+            else
+            {
+                player1 = new Computer();
+            }
+            Console.Clear();
+
+            Console.WriteLine("Выберите кто отгадывает число:\n" +
+                "1. Человек\n" +
+                "2. Машина");
+            choise = int.Parse(Console.ReadLine());
+
+            if (choise == 1)
+            {
+                player2 = new Human();
+            }
+            else
+            {
+                player2 = new Computer();
+            }
+
+            number = player1.MakeAGuess();
+            player2.Guess(number, lives);
+        }
+    }
+}
