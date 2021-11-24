e94bcd0d6c4fbb8f81f118123a1ac75617fbc31c
Zhukovskiy Dmytro <dimazhykovskiy@gmail.com>
2021-11-24 21:49:58 +0200

    Numbers and plus buttons

--- WPF_Calc/MainWindow.xaml.cs
+++ WPF_Calc/MainWindow.xaml.cs
@@ -20,9 +20,51 @@ namespace WPF_Calc
     /// </summary>
     public partial class MainWindow : Window
     {
+        int num1 = 0;
+        int num2 = 0;
+        string operation = "";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btn_num_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            String str = button.Content.ToString();
+            int num = Int32.Parse(str);
+            if(operation == "")
+            {
+                num1 = num1 * 10 + num;
+                txtValue.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = num2 * 10 + num;
+                txtValue.Text = num2.ToString();
+            }
+        }
+
+        private void btn_plus_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "+";
+            txtValue.Text = "";
+        }
+
+        private void btn_eq_Click(object sender, RoutedEventArgs e)
+        {
+            int result = 0;
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                default:
+                    break;
+            }
+            txtValue.Text = result.ToString();
+            operation = "";
+            num1 = result;
         }
     }
 }
